89318bdab3973c09914aa77de09f7b219beab733
AmericoKutomi <americo@embra.com.br>
2026-07-23 11:22:18 -0300

    W02 Code Queues correction

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,10 +6,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: The Dequeue function shall remove the item with 
-    //   the highest priority and return its value.
-    // Expected Result: The high priority item should come first
-    // Defect(s) Found: The high priority item doesn't come
+    // Scenario: The Dequeue function shall return the item with 
+    //   the highest priority. It is in the middle of the queue.
+    // Expected Result: def (The high priority item should come)
+    // Defect(s) Found: None.
     public void TestPriorityQueue_1()
     {
         // arrange
@@ -32,8 +32,8 @@ public class PriorityQueueTests
     // Scenario: If the queue is empty, then an error exception shall be thrown. 
     //   This exception should be an InvalidOperationException with a message of 
     //   "The queue is empty."
-    // Expected Result: Exception shall be thrown.
-    // Defect(s) Found: The empty queue is not considered
+    // Expected Result: Exception shall be thrown with message "The queue is empty."
+    // Defect(s) Found: None.
     public void TestPriorityQueue_2()
     {
         // arrange
@@ -51,4 +51,93 @@ public class PriorityQueueTests
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: The item with the highest priority is at the beginning 
+    //   of the list.
+    // Expected Result: abc
+    // Defect(s) Found: None.
+    public void TestHighestPriorityAtBeginning()
+    {
+        // arrange
+        var priorityQueue = new PriorityQueue();
+
+        // act
+        var abc = new PriorityItem("abc", 3);  // this is the highest
+        var def = new PriorityItem("def", 2);
+        var ghi = new PriorityItem("ghi", 2);
+
+        priorityQueue.Enqueue(abc.Value, abc.Priority);
+        priorityQueue.Enqueue(def.Value, def.Priority);
+        priorityQueue.Enqueue(ghi.Value, ghi.Priority);
+
+        // assert
+        Assert.AreEqual(abc.Value, priorityQueue.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: The item with the highest priority is at the end 
+    //   of the list.
+    // Expected Result: ghi
+    // Defect(s) Found: def; the last element didn't come
+    public void TestHighestPriorityAtEnd()
+    {
+        // arrange
+        var priorityQueue = new PriorityQueue();
+
+        // act
+        var abc = new PriorityItem("abc", 2);
+        var def = new PriorityItem("def", 2);
+        var ghi = new PriorityItem("ghi", 3);  // this is the highest
+
+        priorityQueue.Enqueue(abc.Value, abc.Priority);
+        priorityQueue.Enqueue(def.Value, def.Priority);
+        priorityQueue.Enqueue(ghi.Value, ghi.Priority);
+
+        // assert
+        Assert.AreEqual(ghi.Value, priorityQueue.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: The queue has only one element 
+    // Expected Result: abc
+    // Defect(s) Found: None.
+    public void TestHighestPriorityWithOne()
+    {
+        // arrange
+        var priorityQueue = new PriorityQueue();
+
+        // act
+        var abc = new PriorityItem("abc", 2);  // this is the only one
+
+        priorityQueue.Enqueue(abc.Value, abc.Priority);
+
+        // assert
+        Assert.AreEqual(abc.Value, priorityQueue.Dequeue());
+    }
+
+    [TestMethod]
+    // Scenario: Dequeue is called twice to test remove.
+    //  The highest come first, then the second one.
+    // Expected Result: def first, ghi next
+    // Defect(s) Found: def first, def again. Remove is not working.
+    public void TestHighestPriorityTwoDequeues()
+    {
+        // arrange
+        var priorityQueue = new PriorityQueue();
+
+        // act
+        var abc = new PriorityItem("abc", 1);
+        var def = new PriorityItem("def", 3);  // this is the highest
+        var ghi = new PriorityItem("ghi", 2);  // this is the second
+
+        priorityQueue.Enqueue(abc.Value, abc.Priority);
+        priorityQueue.Enqueue(def.Value, def.Priority);
+        priorityQueue.Enqueue(ghi.Value, ghi.Priority);
+
+        // assert
+        Assert.AreEqual(def.Value, priorityQueue.Dequeue());
+        Assert.AreEqual(ghi.Value, priorityQueue.Dequeue());
+    }
+
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: None.
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         // Arrange
@@ -52,7 +52,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: None.
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         // Arrange
@@ -108,7 +108,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: People with zero turns were removed from the queue instead of remaining in the queue.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         // Arrange
@@ -148,7 +148,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: People with negative turns were removed from the queue instead of remaining in the queue.
     public void TestTakingTurnsQueue_ForeverNegative()
     {
         // Arrange
@@ -187,7 +187,7 @@ public class TakingTurnsQueueTests
     [TestMethod]
     // Scenario: Try to get the next person from an empty queue
     // Expected Result: Exception should be thrown with appropriate error message.
-    // Defect(s) Found: 
+    // Defect(s) Found: None.
     public void TestTakingTurnsQueue_Empty()
     {
         // Act
